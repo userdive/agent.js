e6d22c4ac08828d190992524166bfefb08792b5b
Keisuke Kan <kan@uncovertruth.co.jp>
2016-11-14 16:42:47 +0900

    chore(test) use flowtype

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
+/* @flow */
 module.exports = {
   'extends': ['@uncovertruth/eslint-config-flowtype']
 }
--- test/setup.js
+++ test/setup.js
@@ -1,3 +1,4 @@
+/* @flow */
 const jsdom = require('jsdom').jsdom
 
 global.document = jsdom('')
