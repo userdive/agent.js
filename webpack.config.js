6dcf59dcf9e5851f4818e9dee90040e5a46d5349
Keisuke Kan <kan@uncovertruth.co.jp>
2017-01-04 17:00:06 +0900

    refactor(webpack) use FullYear

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 /* @flow */
 const ClosureCompilerPlugin = require('webpack-closure-compiler')
 const webpack = require('webpack')
-const moment = require('moment')
 const path = require('path')
 const version = require('./package.json').version
+
+const date = new Date()
 
 module.exports = {
   entry: {
@@ -25,6 +26,6 @@ module.exports = {
       },
       concurrency: 3
     }),
-    new webpack.BannerPlugin(`@userdive/agent.js ${version} | Copyright (c) ${moment().format('YYYY')} UNCOVER TRUTH Inc.`)
+    new webpack.BannerPlugin(`@userdive/agent.js ${version} | Copyright (c) ${date.getFullYear()} UNCOVER TRUTH Inc.`)
   ]
 }
