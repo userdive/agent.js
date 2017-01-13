84e36aeebc62c84efb336048f5c9e026c4c40cbf
Keisuke Kan <kan@uncovertruth.co.jp>
2017-01-13 19:36:05 +0900

    feat(test/events) add Click test

---
+++ test/events/click.test.js
@@ -0,0 +1,30 @@
+/* @flow */
+import { describe, it, beforeEach } from 'mocha'
+import { spy as sinonSpy } from 'sinon'
+import assert from 'assert'
+
+import { OPTIONS, POINT } from '../../src/constants'
+
+describe('click', () => {
+  const EventEmitter = require('events').EventEmitter
+  const ClickEvents = require('../../src/events/click').default
+  const Logger = require('../../src/logger').default
+
+  let emitter, logger, instance, spy
+
+  beforeEach(() => {
+    spy = sinonSpy()
+    logger = new Logger(OPTIONS.Raven)
+    emitter = new EventEmitter()
+    emitter.on(POINT, spy)
+    instance = new ClickEvents(emitter, logger)
+  })
+
+  it('bind', () => {
+    instance.bind()
+    const e = document.createEvent('MouseEvents')
+    e.initEvent('click', false, true)
+    document.dispatchEvent(e)
+    assert(spy.calledOnce)
+  })
+})
