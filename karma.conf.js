76bb0cb44bcceda9b7d8490ebe69bd95cfb9a8c9
Keisuke Kan <kan@uncovertruth.co.jp>
2017-01-17 21:10:16 +0900

    chore(package) update npm-run-all

--- karma.conf.js
+++ karma.conf.js
@@ -1,9 +1,23 @@
+const config = require('./webpack.config')
+
 module.exports = function (config) {
   config.set({
     frameworks: ['mocha'],
     files: [
+      'test/**/*.test.js'
     ],
     preprocessors: {
+      'test/**/*test.js': ['webpack']
+    },
+    webpack: {
+      module: config.module
+    },
+    webpackMiddleware: {
+      noInfo: true,
+      quiet: true,
+      stats: {
+        colors: true
+      }
     },
     reporters: ['progress', 'coverage'],
     browsers: ['Chrome'],
