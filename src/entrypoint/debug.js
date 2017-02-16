1b7c5743f1b4826c6c6201665ab58afcc938dc3a
Keisuke Kan <kan@uncovertruth.co.jp>
2017-02-16 14:55:00 +0900

    chore(src) fix inject options

--- src/entrypoint/debug.js
+++ src/entrypoint/debug.js
@@ -3,10 +3,9 @@ import Raven from 'raven-js'
 import factory from '../global'
 import Agent from '../agent'
 
-export class DebugAgent extends Agent {
+class DebugAgent extends Agent {
   static create (projectId: string, options: any) {
-    Object.assign({}, options, {Raven: Raven})
-    return super.create(projectId, options)
+    return super.create(projectId, Object.assign({}, options, {Raven: Raven}))
   }
 }
 
