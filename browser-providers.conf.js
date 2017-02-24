4473eafefa5010dad4b54e0caf7db645069a3801
Keisuke Kan <kan@uncovertruth.co.jp>
2017-02-24 20:13:28 +0900

    chore(browser-providers) add safari 7

--- browser-providers.conf.js
+++ browser-providers.conf.js
@@ -6,6 +6,7 @@ const customLaunchers = {
   'SL_EDGE': {base: 'SauceLabs', browserName: 'MicrosoftEdge', platform: 'Windows 10', version: '13'},
   'SL_FIREFOX': {base: 'SauceLabs', browserName: 'firefox', platform: 'Windows 10', version: '51'},
   'SL_IE11': {base: 'SauceLabs', browserName: 'internet explorer', platform: 'Windows 8.1', version: '11'},
+  'SL_SAFARI7': {base: 'SauceLabs', browserName: 'safari', platform: 'OS X 10.9', version: '7.0'},
   'SL_SAFARI10': {base: 'SauceLabs', browserName: 'safari', platform: 'OS X 10.11', version: '10.0'},
   'SL_ANDROID4.4': {base: 'SauceLabs', browserName: 'android', version: '4.4'},
   'SL_ANDROID5': {base: 'SauceLabs', browserName: 'android', version: '5.1'},
