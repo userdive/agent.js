2f9bd2962e26912e9d362f77260ccbe848d92ff7
Keisuke Kan <kan@uncovertruth.co.jp>
2017-02-24 22:59:45 +0900

    fix(browser-providers) add platform

--- browser-providers.conf.js
+++ browser-providers.conf.js
@@ -2,8 +2,8 @@
 
 const customLaunchers = {
   'SL_IOS10': {base: 'SauceLabs', browserName: 'iphone', version: '10.0'},
-  'SL_IOS9': {base: 'SauceLabs', browserName: 'iphone', version: '9.0'},
-  'SL_IOS8': {base: 'SauceLabs', browserName: 'iphone', version: '8.0'},
+  'SL_IOS9': {base: 'SauceLabs', browserName: 'iphone', platform: 'OS X 10.10', version: '9.3'},
+  'SL_IOS8': {base: 'SauceLabs', browserName: 'iphone', platform: 'OS X 10.10', version: '8.4'},
   'SL_ANDROID4.4': {base: 'SauceLabs', browserName: 'android', version: '4.4'},
   'SL_ANDROID5': {base: 'SauceLabs', browserName: 'android', version: '5.1'},
   'SL_CHROME46': {base: 'SauceLabs', browserName: 'chrome', platform: 'Linux', version: '46'},
