3ecd89fe520941bb64d7cac5fcd738a2a6ba82d3
Keisuke Kan <kan@uncovertruth.co.jp>
2017-02-24 18:20:14 +0900

    chore(browser-providers) add beta

--- browser-providers.conf.js
+++ browser-providers.conf.js
@@ -2,13 +2,15 @@
 
 const customLaunchers = {
   'SL_CHROME': {base: 'SauceLabs', browserName: 'chrome', version: '56'},
-  'SL_EDGE': {base: 'SauceLabs', browserName: 'MicrosoftEdge', platform: 'Windows 10', version: '13'},
+  'SL_CHROME_BETA': {base: 'SauceLabs', browserName: 'chrome', version: 'beta'},
+  'SL_EDGE': {base: 'SauceLabs', browserName: 'MicrosoftEdge', version: '13'},
   'SL_FIREFOX': {base: 'SauceLabs', browserName: 'firefox', version: '51'},
-  'SL_IE11': {base: 'SauceLabs', browserName: 'internet explorer', platform: 'Windows 10', version: '11'},
-  'SL_SAFARI10': {base: 'SauceLabs', browserName: 'safari', platform: 'OS X 10.12', version: '10.0'},
-  'SL_ANDROID4.4': {base: 'SauceLabs', browserName: 'android', platform: 'Linux', version: '4.4'},
-  'SL_ANDROID5': {base: 'SauceLabs', browserName: 'android', platform: 'Linux', version: '5.1'},
-  'SL_IOS10': {base: 'SauceLabs', browserName: 'iphone', platform: 'OS X 10.10', version: '10.0'}
+  'SL_FIREFOX_BETA': {base: 'SauceLabs', browserName: 'firefox', version: 'beta'},
+  'SL_IE11': {base: 'SauceLabs', browserName: 'internet explorer', version: '11'},
+  'SL_SAFARI10': {base: 'SauceLabs', browserName: 'safari', version: '10.0'},
+  'SL_ANDROID4.4': {base: 'SauceLabs', browserName: 'android', version: '4.4'},
+  'SL_ANDROID5': {base: 'SauceLabs', browserName: 'android', version: '5.1'},
+  'SL_IOS10': {base: 'SauceLabs', browserName: 'iphone', version: '10.0'}
 }
 
 module.exports = {
