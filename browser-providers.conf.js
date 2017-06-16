6f4a1a5b5b3088b86add1100157f0dafa522556a
Keisuke Kan <kan@uncovertruth.co.jp>
2017-06-16 17:37:36 +0900

    fix(src) skip zero index data

--- browser-providers.conf.js
+++ browser-providers.conf.js
@@ -1,22 +1,22 @@
 /* @flow */
 
-const customLaunchers = {
-  SL_IOS10: {base: 'SauceLabs', browserName: 'iphone', version: '10.0'},
-  SL_ANDROID5: {base: 'SauceLabs', browserName: 'android', version: '5.1'},
+exports.customLaunchers = {
+  SL_IOS10: { base: 'SauceLabs', browserName: 'iphone', version: '10.0' },
+  SL_ANDROID5: { base: 'SauceLabs', browserName: 'android', version: '5.1' },
   SL_CHROME46: {
     base: 'SauceLabs',
     browserName: 'chrome',
     platform: 'Linux',
     version: '46'
   },
-  SL_CHROME_BETA: {base: 'SauceLabs', browserName: 'chrome', version: 'beta'},
+  SL_CHROME_BETA: { base: 'SauceLabs', browserName: 'chrome', version: 'beta' },
   SL_FIREFOX: {
     base: 'SauceLabs',
     browserName: 'firefox',
     platform: 'Linux',
     version: '44'
   },
-  SL_SAFARI10: {base: 'SauceLabs', browserName: 'safari', version: '10.0'},
+  SL_SAFARI10: { base: 'SauceLabs', browserName: 'safari', version: '10.0' },
   SL_IE11: {
     base: 'SauceLabs',
     browserName: 'internet explorer',
@@ -29,8 +29,4 @@ const customLaunchers = {
     platform: 'Windows 10',
     version: '14.14393'
   }
-}
-
-module.exports = {
-  customLaunchers
 }
