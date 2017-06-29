31c8a6d3594a93f7a98668b1d838b159767ef689
Keisuke Kan <9renpoto@gmail.com>
2017-06-29 14:30:53 +0900

    chore(src/events) add tap filter (#103)

--- src/events/touch.js
+++ src/events/touch.js
@@ -3,17 +3,49 @@ import EventBase from '../events'
 import { validate } from '../browser'
 import { TOUCH } from '../constants'
 
+function getFirstTouch (e: TouchEvent): Touch {
+  return e.changedTouches ? e.changedTouches[0] : e.touches[0]
+}
+
 export default class TouchEvents extends EventBase {
+  start: Touch
   validate (): boolean {
     return validate(TOUCH)
   }
-  dispatch (e: TouchEvent) {
-    const t = e.changedTouches ? e.changedTouches[0] : e.touches[0]
-    this.emit({ x: t.pageX, y: t.pageY })
-  }
   on () {
-    super.on('touchstart', this.dispatch.bind(this), 'l')
-    super.on('touchmove', this.dispatch.bind(this), 'l')
-    super.on('touchend', this.dispatch.bind(this), 'a')
+    super.on(
+      'touchstart',
+      (e: TouchEvent) => {
+        this.start = getFirstTouch(e)
+        this.emit({ x: this.start.pageX, y: this.start.pageY })
+      },
+      'l'
+    )
+
+    super.on(
+      'touchmove',
+      (e: TouchEvent) => {
+        const t = getFirstTouch(e)
+        this.emit({ x: t.pageX, y: t.pageY })
+      },
+      'l'
+    )
+
+    super.on(
+      'touchend',
+      (e: TouchEvent) => {
+        const t = getFirstTouch(e)
+        if (!this.start) {
+          return this.warning(`start is not defined`)
+        }
+        if (
+          Math.abs(this.start.pageX - t.pageX) < 10 &&
+          Math.abs(this.start.pageY - t.pageY) < 10
+        ) {
+          this.emit({ x: t.pageX, y: t.pageY })
+        }
+      },
+      'a'
+    )
   }
 }
