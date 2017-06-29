d545d80ae37222d1278ff3ba3c55ef32e6938962
Keisuke Kan <kan@uncovertruth.co.jp>
2017-06-29 11:37:48 +0900

    fix(src/events) this is maigo

--- src/events/touch.js
+++ src/events/touch.js
@@ -8,13 +8,12 @@ export default class TouchEvents extends EventBase {
     return validate(TOUCH)
   }
   dispatch (e: TouchEvent) {
-    const self = this
     const t = e.changedTouches ? e.changedTouches[0] : e.touches[0]
-    self.emit({ x: t.pageX, y: t.pageY })
+    this.emit({ x: t.pageX, y: t.pageY })
   }
   on () {
-    super.on('touchstart', this.dispatch, 'l')
-    super.on('touchmove', this.dispatch, 'l')
-    super.on('touchend', this.dispatch, 'a')
+    super.on('touchstart', this.dispatch.bind(this), 'l')
+    super.on('touchmove', this.dispatch.bind(this), 'l')
+    super.on('touchend', this.dispatch.bind(this), 'a')
   }
 }
