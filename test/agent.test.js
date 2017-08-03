8f8a3dbb5bbb4513200d6be15666372080a98cc9
Shinya Ohyanagi <heavenshell.jp@gmail.com>
2017-08-03 20:40:38 +0900

    Fix use stub to check active flag

--- test/agent.test.js
+++ test/agent.test.js
@@ -1,8 +1,9 @@
 /* @flow */
-import {describe, it} from 'mocha'
-import {random, internet} from 'faker'
-import {stub} from 'sinon'
+import { describe, it } from 'mocha'
+import { random, internet } from 'faker'
+import { stub } from 'sinon'
 import assert from 'assert'
+import * as req from '../src/requests'
 
 describe('agent', () => {
   const AgentCore = require('../src/core').default
@@ -10,18 +11,29 @@ describe('agent', () => {
   const agent = new Agent()
 
   it('create', () => {
-    assert(agent.create(random.uuid(), {baseUrl: internet.url()}))
+    assert(agent.create(random.uuid(), { baseUrl: internet.url() }))
   })
 
   it('send', () => {
     const listen = stub(AgentCore.prototype, 'listen')
-    assert(agent.send('pageview').active)
-    assert(agent.send('pageview').active, 'take2')
+
+    const stubGet = (url, query, onload, onerror) => {
+      onload()
+    }
+    const get = stub(req, 'get').callsFake(stubGet)
+
+    let core = agent.send('pageview')
+    assert(core.active)
+
+    core = agent.send('pageview')
+    assert(core.active, 'take2')
+
     listen.restore()
+    get.restore()
   })
 
   it('set', () => {
     assert(agent.set('key', 'value'))
-    assert(agent.set({key: 'value'}))
+    assert(agent.set({ key: 'value' }))
   })
 })
