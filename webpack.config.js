f1001a38a7060a425799886912fc67bfae420dea
Shuta Kumano <shoota.dev@gmail.com>
2017-09-28 09:35:59 +0900

    tweak tyupescript environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,35 @@
-/* @flow */
-const webpack = require('webpack')
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
-const path = require('path')
-const { version, name, author, license } = require('./package.json')
+// TODO move to typescript
+const webpack = require("webpack");
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+const path = require("path");
+const { version, name, author, license } = require("./package.json");
 
-const date = new Date()
+const date = new Date();
 
 module.exports = {
   entry: {
-    'agent.d': path.join(__dirname, 'src/entrypoint/debug.ts'),
-    agent: path.join(__dirname, 'src/entrypoint/index.ts')
+    "agent.d": path.join(__dirname, "src/entrypoint/debug.ts"),
+    agent: path.join(__dirname, "src/entrypoint/index.ts")
   },
   output: {
-    path: path.join(__dirname, 'lib/build/'),
-    filename: '[name].js',
-    sourceMapFilename: '[name].js.map'
+    path: path.join(__dirname, "lib/build/"),
+    filename: "[name].js",
+    sourceMapFilename: "[name].js.map"
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js']
+    extensions: [".ts", ".tsx", ".js"]
   },
-  devtool: 'cheap-source-map',
+  devtool: "cheap-source-map",
   module: {
-    loaders: [
-      { test: /\.tsx?$/, loader: 'ts-loader' }
-    ],
     rules: [
-      { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.tsx?$/, loader: "ts-loader" },
+      { test: /\.js$/, use: "babel-loader", exclude: /node_modules/ },
       {
         test: /logger\.js$/,
-        loader: 'string-replace-loader',
+        loader: "string-replace-loader",
         exclude: /node_modules/,
         options: {
-          search: 'USERDIVE_AGENT_VERSION',
+          search: "USERDIVE_AGENT_VERSION",
           replace: version
         }
       }
@@ -40,7 +38,7 @@ module.exports = {
   plugins: [
     new webpack.optimize.ModuleConcatenationPlugin(),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
     }),
     new UglifyJSPlugin({
       sourceMap: true,
@@ -50,4 +48,4 @@ module.exports = {
       banner: `${name} ${version} | Copyright (c) ${date.getFullYear()} ${author} | License ${license}`
     })
   ]
-}
+};
