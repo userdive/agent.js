9ba390ce7c44cc7395845dd449c228607c33e4d3
Keisuke Kan <9renpoto@gmail.com>
2017-11-03 01:16:57 +0900

    fix(browser-providers) skip connection failed browsers (#151)

--- browser-providers.conf.ts
+++ browser-providers.conf.ts
@@ -14,17 +14,17 @@ export const customLaunchers = {
     platform: 'Windows 10',
     version: '51'
   },
-  SL_SAFARI10: { base: 'SauceLabs', browserName: 'safari', version: '10.0' },
+  // SL_SAFARI10: { base: 'SauceLabs', browserName: 'safari', version: '10.0' },
   SL_IE11: {
     base: 'SauceLabs',
     browserName: 'internet explorer',
     platform: 'Windows 8.1',
     version: '11'
-  },
-  SL_EDGE: {
-    base: 'SauceLabs',
-    browserName: 'MicrosoftEdge',
-    platform: 'Windows 10',
-    version: '14.14393'
+    //   },
+    //   SL_EDGE14: {
+    //     base: 'SauceLabs',
+    //     browserName: 'MicrosoftEdge',
+    //     platform: 'Windows 10',
+    //     version: '14.14393'
   }
 }
