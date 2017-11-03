28dd4ae0aff78a098197f81aaa83021326477224
Keisuke Kan <kan@uncovertruth.co.jp>
2017-11-03 13:49:13 +0900

    chore(browser-providers) skip connection failed browsers

--- browser-providers.conf.ts
+++ browser-providers.conf.ts
@@ -1,6 +1,6 @@
 export const customLaunchers = {
-  // SL_IOS10: { base: 'SauceLabs', browserName: 'iphone', version: '10.0' },
-  // SL_IOS11: { base: 'SauceLabs', browserName: 'iphone', version: '11.0' },
+  SL_IOS10: { base: 'SauceLabs', browserName: 'iphone', version: '10.0' },
+  SL_IOS11: { base: 'SauceLabs', browserName: 'iphone', version: '11.0' },
   SL_ANDROID5: { base: 'SauceLabs', browserName: 'android', version: '5.1' },
   SL_ANDROID6: {
     base: 'SauceLabs',
@@ -29,24 +29,18 @@ export const customLaunchers = {
     platform: 'Windows 10',
     version: '51'
   },
-  SL_FIREFOXDEV: {
-    base: 'SauceLabs',
-    platform: 'Windows 10',
-    browserName: 'firefox',
-    version: 'dev'
-  },
-  SL_SAFARI10: {
-    base: 'SauceLabs',
-    browserName: 'safari',
-    platform: 'OS X 10.12',
-    version: '10.0'
-  },
-  SL_SAFARI11: {
-    base: 'SauceLabs',
-    browserName: 'safari',
-    platform: 'OS X 10.12',
-    version: '10.0'
-  },
+  // SL_SAFARI10: {
+  //   base: 'SauceLabs',
+  //   browserName: 'safari',
+  //   platform: 'OS X 10.12',
+  //   version: '10.0'
+  // },
+  // SL_SAFARI11: {
+  //   base: 'SauceLabs',
+  //   browserName: 'safari',
+  //   platform: 'OS X 10.12',
+  //   version: '10.0'
+  // },
   SL_IE11: {
     base: 'SauceLabs',
     browserName: 'internet explorer',
