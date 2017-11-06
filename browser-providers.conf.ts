564b31ec3b350cf7a1236e82af4ef82b2f43d5d4
Keisuke Kan <9renpoto@gmail.com>
2017-11-06 09:34:32 +0900

    chore(browser-providers) Added new versions (#152)
    * fix(browser-providers) revert platforms
    
    * fix(browser-providers) added platform
    
    * refactor(karma.conf) down timeout
    
    * chore(browser-providers) Added FireFox beta
    
    * chore(browser-providers) Added FireFox dev
    
    * chore(browser-providers) skip connection failed browsers

--- browser-providers.conf.ts
+++ browser-providers.conf.ts
@@ -1,6 +1,21 @@
 export const customLaunchers = {
   SL_IOS10: { base: 'SauceLabs', browserName: 'iphone', version: '10.0' },
+  SL_IOS11: { base: 'SauceLabs', browserName: 'iphone', version: '11.0' },
   SL_ANDROID5: { base: 'SauceLabs', browserName: 'android', version: '5.1' },
+  SL_ANDROID6: {
+    base: 'SauceLabs',
+    browserName: 'Chrome',
+    platform: 'Android',
+    version: '6.0',
+    device: 'Android Emulator'
+  },
+  SL_ANDROID7: {
+    base: 'SauceLabs',
+    browserName: 'Chrome',
+    platform: 'Android',
+    version: '7.1',
+    device: 'Android GoogleAPI Emulator'
+  },
   SL_CHROME46: {
     base: 'SauceLabs',
     browserName: 'chrome',
@@ -14,17 +29,28 @@ export const customLaunchers = {
     platform: 'Windows 10',
     version: '51'
   },
-  // SL_SAFARI10: { base: 'SauceLabs', browserName: 'safari', version: '10.0' },
+  // SL_SAFARI10: {
+  //   base: 'SauceLabs',
+  //   browserName: 'safari',
+  //   platform: 'OS X 10.12',
+  //   version: '10.0'
+  // },
+  // SL_SAFARI11: {
+  //   base: 'SauceLabs',
+  //   browserName: 'safari',
+  //   platform: 'OS X 10.12',
+  //   version: '10.0'
+  // },
   SL_IE11: {
     base: 'SauceLabs',
     browserName: 'internet explorer',
     platform: 'Windows 8.1',
     version: '11'
-    //   },
-    //   SL_EDGE14: {
-    //     base: 'SauceLabs',
-    //     browserName: 'MicrosoftEdge',
-    //     platform: 'Windows 10',
-    //     version: '14.14393'
   }
+  // SL_EDGE14: {
+  //   base: 'SauceLabs',
+  //   browserName: 'MicrosoftEdge',
+  //   platform: 'Windows 10',
+  //   version: '14.14393'
+  // }
 }
