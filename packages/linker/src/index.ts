25847eeb9a9a8bf3686adf088d5f5f635892cdbc
Shuta Kumano <shoota.dev@gmail.com>
2018-01-17 10:23:15 +0900

    add linker package

---
+++ packages/linker/src/index.ts
@@ -0,0 +1,3 @@
+function linker () {
+  return true
+}
---
+++ packages/linker/webpack.config.ts
@@ -0,0 +1,52 @@
+const webpack = require('webpack')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const path = require('path')
+const { version, name: moduleName, author, license } = require('./package.json')
+
+const date = new Date()
+
+module.exports = {
+  entry: {
+    linker: path.join(__dirname, 'src/index.ts')
+  },
+  output: {
+    path: path.join(__dirname, 'lib/build/'),
+    filename: '[name].js',
+    sourceMapFilename: '[name].js.map'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  devtool: 'cheap-source-map',
+  module: {
+    rules: [
+      { test: /\.ts$/, loader: 'ts-loader' },
+      {
+        test: /logger\.ts$/,
+        loader: 'string-replace-loader',
+        exclude: /node_modules/,
+        options: {
+          search: 'USERDIVE_AGENT_VERSION',
+          replace: version
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    new UglifyJSPlugin({
+      sourceMap: true,
+      uglifyOptions: {
+        output: {
+          comments: false
+        }
+      }
+    }),
+    new webpack.BannerPlugin({
+      banner: `${moduleName} ${version} | Copyright (c) ${date.getFullYear()} ${author} | License ${license}`
+    })
+  ]
+}
