ca35db3c12c806d723e7f4411c15c73c45ec2cbc
Keisuke Kan <kan@uncovertruth.co.jp>
2018-03-01 10:35:42 +0900

    feat(packages) Merge userdive.js

---
+++ packages/userdive/README.md
@@ -0,0 +1,30 @@
+# userdive
+
+[![npm version](https://badge.fury.io/js/userdive.svg)](https://www.npmjs.com/package/userdive)
+
+> TBA
+
+## Table of Contents
+
+*   [Install](#install)
+*   [Usage](#usage)
+*   [Contribute](#contribute)
+*   [License](#license)
+
+## Install
+
+```sh
+npm install userdive --save
+```
+
+## Usage
+
+> TBA
+
+## Contribute
+
+PRs accepted. [join us](https://www.wantedly.com/companies/uncovertruth/projects)
+
+## License
+
+This library is licensed under GPLv3. Full license text is available in [LICENSE](https://github.com/userdive/userdive.js/blob/master/LICENSE)
---
+++ packages/userdive/src/index.ts
@@ -0,0 +1,44 @@
+/*! userdive | Copyright (c) UNCOVER TRUTH Inc. */
+
+const TAG_NAME = 'script'
+
+export interface IAttributes {
+  [key: string]: any
+}
+
+export function inject (source: string, attributes: IAttributes) {
+  const element: any = document.createElement(TAG_NAME)
+  const script: any = document.getElementsByTagName(TAG_NAME)[0]
+  element.async = 1
+  element.defer = 1
+  element.src = source
+  element.charset = 'UTF-8'
+  Object.keys(attributes).forEach(key => {
+    element.setAttribute(key, attributes[key])
+  })
+  script.parentNode.insertBefore(element, script)
+}
+
+export function q (name: string, global: any) {
+  global[name] =
+    global[name] ||
+    function () {
+      (global[name].q = global[name].q || []).push(arguments)
+    }
+  return global[name]
+}
+
+export default function (
+  name?: string,
+  source?: string,
+  global?: any
+): Function {
+  name = name || '_ud'
+  source = source || 'https://cdn.userdive.com/agent.js'
+  global = global || window
+  if (global[name]) {
+    return global[name]
+  }
+  inject(source, { 'data-ud-namespace': name })
+  return q(name, global)
+}
