18814825c592fcc5ba029d8fa53a512e3253c3be
Keisuke Kan <kan@uncovertruth.co.jp>
2018-03-13 12:36:27 +0900

    fix(textlintrc) format

--- .textlintrc.js
+++ .textlintrc.js
@@ -1,11 +1,13 @@
 const config = require('@uncovertruth/textlint-config-en')
-config.rules["incremental-headers"] = 1
+config.rules['incremental-headers'] = 1
 
 if (process.env.CI) {
-config.rules["no-dead-link"] = Object.assign({}, config.rules["no-dead-link"], {
-    "ignore": [
-      "https://www.wantedly.com/companies/uncovertruth/projects"
-    ]
-})
+  config.rules['no-dead-link'] = Object.assign(
+    {},
+    config.rules['no-dead-link'],
+    {
+      ignore: ['https://www.wantedly.com/companies/uncovertruth/projects']
+    }
+  )
 }
 module.exports = config
--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,41 +1,21 @@
 module.exports = {
   linters: {
-    '*.ts': [
-      'prettier-eslint --write',
-      'tslint --fix',
-      'tslint',
-      'git add'
-    ],
+    '*.ts': ['prettier-eslint --write', 'tslint --fix', 'tslint', 'git add'],
     '*.tsx': [
       'prettier-eslint --eslint-config-path ./website/.eslintrc.json --write',
       'tslint --fix -c ./examples/tslint.json',
       'tslint -c ./examples/tslint.json',
       'git add'
     ],
-    'website/**/*.js': [
+    '*.js': [
       'prettier-eslint --eslint-config-path ./website/.eslintrc.json --write',
       'eslint ./website/.eslintrc.json',
       'git add'
     ],
-    '*.pug': [
-      'pug-lint'
-    ],
-    'package.json': [
-      'fixpack',
-      'git add'
-    ],
-    '*.md': [
-      'prettier --write',
-      'textlint',
-      'git add'
-    ],
-    '*.css': [
-      'prettier --write',
-      'stylelint --fix',
-      'git add'
-    ]
+    '*.pug': ['pug-lint'],
+    'package.json': ['fixpack', 'git add'],
+    '*.md': ['prettier --write', 'textlint', 'git add'],
+    '*.css': ['prettier --write', 'stylelint --fix', 'git add']
   },
-  'ignore': [
-    'CHANGELOG.md'
-  ]
+  ignore: ['CHANGELOG.md']
 }
