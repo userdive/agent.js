1d302603de1346a548d9a4d3d3326785bee6cd75
Keisuke Kan <kan@uncovertruth.co.jp>
2018-03-13 12:28:44 +0900

    fix(textlintrc) Added ignore flag

--- .textlintrc.js
+++ .textlintrc.js
@@ -1,3 +1,11 @@
 const config = require('@uncovertruth/textlint-config-en')
 config.rules["incremental-headers"] = 1
+
+if (process.env.CI) {
+config.rules["no-dead-link"] = Object.assign({}, config.rules["no-dead-link"], {
+    "ignore": [
+      "https://www.wantedly.com/companies/uncovertruth/projects"
+    ]
+})
+}
 module.exports = config
