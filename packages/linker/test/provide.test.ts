a2f8ad4aef4df440006ea12a21dc776304171d50
Keisuke Kan <kan@uncovertruth.co.jp>
2018-03-27 19:44:14 +0900

    refactor(packages/linker/test) cached global

--- packages/linker/test/provide.test.ts
+++ packages/linker/test/provide.test.ts
@@ -8,34 +8,29 @@ import provide from '../src/provide'
 
 describe('provide', () => {
   const name = random.word()
+  const w: any = window
   afterEach(() => {
-    ((w: any) => {
-      w[name] = undefined
-    })(window)
+    w[name] = undefined
   })
 
   it('if not exist queue', () => {
-    ((w: any) => {
-      inject('', { [NAMESPACE]: name })
-      provide('linker', Linker)
-      assert(w[name].q)
-      const provideQueue = w[name].q[0]
-      assert(provideQueue[0] === 'provide')
-      assert(provideQueue[1] === 'linker')
-      assert(provideQueue[2] === Linker)
-    })(window)
+    inject('', { [NAMESPACE]: name })
+    provide('linker', Linker)
+    assert(w[name].q)
+    const provideQueue = w[name].q[0]
+    assert(provideQueue[0] === 'provide')
+    assert(provideQueue[1] === 'linker')
+    assert(provideQueue[2] === Linker)
   })
 
   it('if queue already exists', () => {
-    ((w: any) => {
-      inject('', { [NAMESPACE]: name })
-      w[name] = q(name, window)
-      provide('linker', Linker)
-      assert(w[name].q)
-      const provideQueue = w[name].q[0]
-      assert(provideQueue[0] === 'provide')
-      assert(provideQueue[1] === 'linker')
-      assert(provideQueue[2] === Linker)
-    })(window)
+    inject('', { [NAMESPACE]: name })
+    w[name] = q(name, window)
+    provide('linker', Linker)
+    assert(w[name].q)
+    const provideQueue = w[name].q[0]
+    assert(provideQueue[0] === 'provide')
+    assert(provideQueue[1] === 'linker')
+    assert(provideQueue[2] === Linker)
   })
 })
