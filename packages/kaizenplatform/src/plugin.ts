b0fc5b394bb54c6c1156fcd3fe2d2ade4b68c2ff
Shuta Kumano <shoota.dev@gmail.com>
2018-04-10 11:45:17 +0900

    send ecperimentType as eventAction

--- packages/kaizenplatform/src/plugin.ts
+++ packages/kaizenplatform/src/plugin.ts
@@ -9,12 +9,16 @@ export default class Plugin {
   getVariation () {
     (q('kzs', window) as any)(
       'getVariation',
-      ({ variationId }: { variationId: string }, state: string) => {
+      (
+        data: { experimentType: string; variationId: string },
+        state: string
+      ) => {
         try {
           if (state === 'decided') {
             this.tracker.send('event', {
               eventCategory: 'kaizenplatform',
-              eventLabel: `?_kzs_var_id=${variationId}`
+              eventAction: data.experimentType,
+              eventLabel: data.variationId
             })
           }
         } catch (e) {
