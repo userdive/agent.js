69b9e22e140c76ff0e5dcec14024eac6c167c238
Shuta Kumano <shoota.dev@gmail.com>
2018-05-15 11:34:29 +0900

    add kzs unit test

---
+++ packages/kaizenplatform/test/kzs.test.ts
@@ -0,0 +1,20 @@
+import Agent from '@userdive/agent'
+import * as assert from 'assert'
+import { random } from 'faker'
+import 'mocha'
+import { spy as sinonSpy } from 'sinon'
+import Kzs from '../src/plugin'
+
+describe('kzs', () => {
+  let agent: any
+  let kzs: Kzs
+  beforeEach(() => {
+    agent = new Agent(random.uuid(), 'auto')
+    kzs = new Kzs(agent)
+  })
+
+  it('constructor', () => {
+    const instance: any = kzs
+    assert(instance.tracker)
+  })
+})
