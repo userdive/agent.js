dd8cc10094d12d4db371d0be6f9e1ad5db559e3d
Shuta Kumano <shoota.dev@gmail.com>
2018-05-18 09:57:27 +0900

    add linker e2e test

---
+++ examples/plugin-linker/index.test.ts
@@ -0,0 +1,18 @@
+import { NightwatchAPI } from 'nightwatch'
+
+const linkPramKey: string = '?__ud='
+const testPath = '/plugin-linker/'
+const linkSelector: string = '.navbar-nav li:nth-child(2)'
+
+export default {
+  before: (_: any, done: () => void) => {
+    done()
+  },
+  'click link': (browser: NightwatchAPI) =>
+    browser
+      .url(`${browser.launch_url}${testPath}`)
+      .waitForElementVisible('body', 1000)
+      .click(linkSelector)
+      .assert.urlContains(linkPramKey)
+      .end()
+}
