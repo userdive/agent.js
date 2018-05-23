c54f46443d51beba10a0759a94233cd244ee2d2a
Shoota Kumano <shoota.dev@gmail.com>
2018-05-23 11:19:47 +0900

    ignore domains not mandatory service from no-dead-link lint (#587)

--- .textlintrc.js
+++ .textlintrc.js
@@ -4,7 +4,11 @@ config.rules['incremental-headers'] = 1
 config.rules['no-dead-link'] = Object.assign({}, config.rules['no-dead-link'], {
   ignore: [
     'https://www.wantedly.com/companies/uncovertruth/projects',
-    'https://v1.userdive.com'
+    'https://v1.userdive.com',
+    'https://codebeat.co/**/*',
+    'https://app.codacy.com/**/*',
+    'https://www.codefactor.io/**/*',
+    'https://david-dm.org/**/*'
   ]
 })
 
