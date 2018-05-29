30291ee871286cb76c75b6cbe77ee94d10eb9900
Shuta Kumano <shoota.dev@gmail.com>
2018-05-29 16:44:45 +0900

    refactor size-limit config

---
+++ .size-limit.js
@@ -0,0 +1,23 @@
+const dir = 'cdn'
+module.exports = [
+  {
+    path: `${dir}/agent.js`,
+    limit: '8.4 KB'
+  },
+  {
+    path: `${dir}/agent.d.js`,
+    limit: '21 KB'
+  },
+  {
+    path: `${dir}/linker.js`,
+    limit: '1.2 KB'
+  },
+  {
+    path: `${dir}/kaizenplatform-plugin.js`,
+    limit: ' 700 B'
+  },
+  {
+    path: `${dir}/vwo-plugin.js`,
+    limit: ' 810 B'
+  }
+]
