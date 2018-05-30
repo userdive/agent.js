7285beb34045e025c5d3494da28f2499ec0a27f9
Shuta Kumano <shoota.dev@gmail.com>
2018-05-30 11:12:42 +0900

    refactor vwo plugin
    - deep nest
    - too long function

--- packages/vwo/src/plugin.ts
+++ packages/vwo/src/plugin.ts
@@ -2,46 +2,25 @@ import Agent from '@userdive/agent'
 
 export default class Plugin {
   private tracker: Agent
+  private isSent: boolean
+  private tryCount: number
   constructor (tracker: Agent) {
     this.tracker = tracker
+    this.init()
+  }
+
+  public init = () => {
+    this.isSent = false
+    this.tryCount = 0
   }
 
   public getVariation (global: any, pollInterval?: number, maxTry?: number) {
     const interval: number = pollInterval || 200
     const max: number = maxTry || 10
-    let isSent = false
-    let tryCount = 0
     const sendVariation = (q: any[], vwoExpIds: string[], vwoExp: any[]) => {
-      const sendEvent = () => {
-        try {
-          for (let i = 0; i < vwoExpIds.length; i++) {
-            const visId: string = vwoExpIds[i]
-            const exp = vwoExp[parseInt(vwoExpIds[i], 10)]
-            if (!exp.ready) {
-              continue
-            }
-            let visCombination: string = global._vis_opt_readCookie(
-              '_vis_opt_exp_' + visId + '_combi'
-            )
-            if (typeof exp.combination_chosen !== 'undefined') {
-              visCombination = exp.combination_chosen
-            }
-            if (typeof exp.comb_n[visCombination] !== 'undefined') {
-              isSent = true
-              this.tracker.send('event', {
-                eventCategory: 'vwo',
-                eventAction: visCombination,
-                eventLabel: visId
-              })
-            }
-          }
-        } catch (e) {
-          // do nothing
-        }
-      }
+      const sendEvent = this.sendEvent(global, vwoExpIds, vwoExp)
       q.push(sendEvent)
     }
-
     const pollingForReady = (): void => {
       if (typeof global._vwo_exp_ids !== 'undefined') {
         sendVariation(
@@ -49,13 +28,43 @@ export default class Plugin {
           global._vwo_exp_ids,
           global._vwo_exp
         )
-        if (tryCount < max && !isSent) {
-          tryCount++
+        if (this.tryCount < max && !this.isSent) {
+          this.tryCount++
           setTimeout(pollingForReady, interval)
         }
       }
     }
 
     pollingForReady()
+  }
+
+  private sendEvent = (global: any, vwoExpIds: string[], vwoExp: any[]) => {
+    return () => {
+      try {
+        for (let i = 0; i < vwoExpIds.length; i++) {
+          const visId: string = vwoExpIds[i]
+          const exp = vwoExp[parseInt(vwoExpIds[i], 10)]
+          if (!exp.ready) {
+            continue
+          }
+          let visCombination: string = global._vis_opt_readCookie(
+            '_vis_opt_exp_' + visId + '_combi'
+          )
+          if (typeof exp.combination_chosen !== 'undefined') {
+            visCombination = exp.combination_chosen
+          }
+          if (typeof exp.comb_n[visCombination] !== 'undefined') {
+            this.isSent = true
+            this.tracker.send('event', {
+              eventCategory: 'vwo',
+              eventAction: visCombination,
+              eventLabel: visId
+            })
+          }
+        }
+      } catch (e) {
+        // do nothing
+      }
+    }
   }
 }
