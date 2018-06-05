1bd7d9545258e795d4c30305c3cb30566c15bda8
Shuta Kumano <shoota.dev@gmail.com>
2018-06-05 15:35:32 +0900

    tweak test assersion

--- examples/test/kaizen-platform.test.ts
+++ examples/test/kaizen-platform.test.ts
@@ -1,3 +1,4 @@
+import * as assert from 'assert'
 import { Selector } from 'testcafe'
 import { baseUrl, deployBaseUrl } from '../testcafe-conf'
 
@@ -13,7 +14,6 @@ fixture('deploy host').page(`${deployBaseUrl}/kaizen-platform/`)
 
 test('display experiment text', async (t) => {
   const nav = Selector('.navbar-brand')
-  await t
-    .wait(1000)
-    .expect(await nav.innerText).eql('USERDIVE Kaizen Platform Integration example')
+  await t.wait(500)
+  assert.equal(await nav.innerText, 'USERDIVE Kaizen Platform Integration example')
 })
