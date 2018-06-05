0da208e9c93034e45392cd2f130f012b4aac4d37
Shuta Kumano <shoota.dev@gmail.com>
2018-06-05 15:54:32 +0900

    separate test with fixture

--- examples/test/kaizen-platform.test.ts
+++ examples/test/kaizen-platform.test.ts
@@ -1,19 +1,11 @@
 import * as assert from 'assert'
 import { Selector } from 'testcafe'
-import { baseUrl, deployBaseUrl } from '../testcafe-conf'
+import { baseUrl } from '../testcafe-conf'
 
 fixture('local host').page(`${baseUrl}/kaizen-platform/`)
 
 test('display original text', async (t) => {
   const nav = Selector('.navbar-brand')
-  await t
-    .expect(await nav.innerText).eql('USERDIVE Integration example')
-})
-
-fixture('deploy host').page(`${deployBaseUrl}/kaizen-platform/`)
-
-test('display experiment text', async (t) => {
-  const nav = Selector('.navbar-brand')
   await t.wait(500)
-  assert.equal(await nav.innerText, 'USERDIVE Kaizen Platform Integration example')
+  assert.equal(await nav.innerText, 'USERDIVE Integration example')
 })
