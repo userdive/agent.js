9444c5a72a8a2d24184eeb8a9e712cdc666d476f
Shuta Kumano <shoota.dev@gmail.com>
2018-06-05 09:36:35 +0900

    good bye nightwatch, hello testcafe

---
+++ examples/testcafe-conf.ts
@@ -0,0 +1,2 @@
+export const baseUrl = 'http://localhost:8080/agent.js'
+export const deployBaseUrl = 'https://userdive.github.io/agent.js'
--- examples/with-angular/with-angular.test.ts
+++
@@ -1,3 +0,0 @@
-import factory from '../spa-base.test'
-const test = factory('/with-angular/#/')
-export default test
