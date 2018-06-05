b013e3c89590444bb89515680206e0f9d5a590b2
Shuta Kumano <shoota.dev@gmail.com>
2018-06-05 17:22:21 +0900

    refactor spa test

--- examples/test/test-util.ts
+++ examples/test/test-util.ts
@@ -1,5 +1,31 @@
-import { ClientFunction } from 'testcafe'
+import * as assert from 'assert'
+import { ClientFunction, Selector } from 'testcafe'
+import { baseUrl } from '../testcafe-conf'
 
 export const getLocation = ClientFunction(() => {
   return document.location.href
 })
+
+export const spaSelector = '#app a'
+export const displayTop = () => {
+  test('should display Top', async (t) => {
+    const link = Selector(spaSelector)
+    await t
+    .expect(await link.innerText).eql('Sample2')
+  })
+}
+
+export const changeToSample2 = (path: string) => {
+  test('should change to Sample2', async (t) => {
+    const link = Selector(spaSelector)
+    await t
+    .click(link)
+    assert.equal(await link.innerText, 'Sample1')
+    assert.equal(await getLocation(), `${baseUrl}${path}sample2`)
+  })
+}
+
+export const spaTest = (path: string) => {
+  displayTop()
+  changeToSample2(path)
+}
--- examples/test/with-angular1.test.ts
+++ examples/test/with-angular1.test.ts
@@ -1,21 +1,7 @@
-import * as assert from 'assert'
-import { Selector } from 'testcafe'
 import { baseUrl } from '../testcafe-conf'
-import { getLocation } from './test-util'
+import { spaTest as test } from './test-util'
 
 const path = '/with-angular1/#!/'
-export const link = Selector('#app a')
 
 fixture('with angular1').page(`${baseUrl}${path}`)
-
-test('should change to Sample2', async (t) => {
-  await t
-  .click(link)
-  assert.equal(await link.innerText, 'Sample1')
-  assert.equal(await getLocation(), `${baseUrl}${path}sample2`)
-})
-
-test('should display Top', async (t) => {
-  await t
-  .expect(await link.innerText).eql('Sample2')
-})
+test(path)
