6f31599c2e1c9475619dd509aec85918c029618b
Shuta Kumano <shoota.dev@gmail.com>
2018-06-06 02:47:54 +0900

    please wait a sec

--- examples/test/kaizen-platform-gtm.test.ts
+++ examples/test/kaizen-platform-gtm.test.ts
@@ -6,6 +6,6 @@ fixture('deploy host').page(`${deployBaseUrl}/kaizen-platform/`)
 
 test('display experiment text', async (t) => {
   const nav = Selector('.navbar-brand')
-  await t.wait(500)
+  await t.wait(2000)
   assert.equal(await nav.innerText, 'USERDIVE Kaizen Platform Integration example')
 })
