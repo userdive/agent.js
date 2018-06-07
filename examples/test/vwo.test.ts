c5b67568d1440c0444368f856993afc935bac35f
Shuta Kumano <shoota.dev@gmail.com>
2018-06-07 09:13:57 +0900

    fix nightwatch

---
+++ examples/test/vwo.test.ts
@@ -0,0 +1,11 @@
+import * as assert from 'assert'
+import { Selector } from 'testcafe'
+import { baseUrl } from '../testcafe-conf'
+
+fixture('local host').page(`${baseUrl}/vwo/`)
+
+test('display original text', async (t) => {
+  const nav = Selector('.navbar-brand')
+  await t.wait(500)
+  assert.equal(await nav.innerText, 'USERDIVE Integration example')
+})
