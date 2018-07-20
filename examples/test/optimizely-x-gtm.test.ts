573a2b48882001a045f7d146e9af4d420e20b53c
Shuta Kumano <shoota.dev@gmail.com>
2018-07-20 16:42:18 +0900

    add e2e test for optimizely x plguin

---
+++ examples/test/optimizely-x-gtm.test.ts
@@ -0,0 +1,11 @@
+import * as assert from 'assert'
+import { Selector } from 'testcafe'
+import { deployBaseUrl } from '../testcafe-conf'
+
+fixture('deploy host').page(`${deployBaseUrl}/optimizely-x/`)
+
+test('display experiment text', async (t) => {
+  const nav = await Selector('.navbar-brand')
+  await t.wait(3000)
+  assert.equal(await nav.innerText, 'Optimizely X Integration example')
+})
